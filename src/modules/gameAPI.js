b03b556d94263c346119c00653f5d98f7bb1becb
Wassim <wassimchakib97@gmail.com>
2022-11-21 22:24:43 +0100

    Add api and show image and title when page loads

--- src/modules/gameAPI.js
+++ src/modules/gameAPI.js
@@ -1,7 +1,14 @@
-const BASE_URL = 'https://www.gamerpower.com/api/giveaways';
+const BASE_URL = 'https://gamerpower.p.rapidapi.com/api/giveaways';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': `${process.env.API_KEY}`,
+    'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com',
+  },
+};
 
 const fetchGames = async () => {
-  const data = await fetch(BASE_URL);
+  const data = await fetch(BASE_URL, options);
   const response = await data.json();
   return response;
 };
