e2401c857ea317760660aa2a5818a57411d76c61
manq2010 <manq2010@gmail.com>
2022-11-22 12:46:34 +0200

    Add comments pop up menu

--- src/modules/involvementAPI.js
+++ src/modules/involvementAPI.js
@@ -1,5 +1,6 @@
 const BASE_URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${process.env.INVOLVEMENT_KEY}`;
 const likeEndpoint = '/likes';
+const commentEndPoint = '/comments';
 
 const getLikes = async () => {
   const data = await fetch(BASE_URL + likeEndpoint);
@@ -7,4 +8,10 @@ const getLikes = async () => {
   return result;
 };
 
-module.exports = { getLikes };
+const getComments = async () => {
+  const data = await fetch(BASE_URL + commentEndPoint);
+  const result = await data.json();
+  return result;
+};
+
+module.exports = { getLikes, getComments };
